0f55c61e36d260f90f11179ce4a220450738a4a9
Kenji Nagai <nagai-kj@nagai-galaxy.com>
2022-02-05 17:39:21 +0900

    3.4.6対応

--- lib/mastodon_command/statuses_controller.rb
+++ lib/mastodon_command/statuses_controller.rb
@@ -3,7 +3,7 @@ module ApiV1StatusesControllerPatch
     status = MastodonCommand.convert_toot(status_params[:status])
     @status = PostStatusService.new.call(current_user.account,
                                          text: status,
-                                         thread: status_params[:in_reply_to_id].blank? ? nil : Status.find(status_params[:in_reply_to_id]),
+                                         thread: @thread,
                                          media_ids: status_params[:media_ids],
                                          sensitive: status_params[:sensitive],
                                          spoiler_text: status_params[:spoiler_text],
@@ -11,7 +11,8 @@ module ApiV1StatusesControllerPatch
                                          scheduled_at: status_params[:scheduled_at],
                                          application: doorkeeper_token.application,
                                          poll: status_params[:poll],
-                                         idempotency: request.headers['Idempotency-Key'])
+                                         idempotency: request.headers['Idempotency-Key'],
+                                         with_rate_limit: true)
 
     render json: @status, serializer: @status.is_a?(ScheduledStatus) ? REST::ScheduledStatusSerializer : REST::StatusSerializer
   end
